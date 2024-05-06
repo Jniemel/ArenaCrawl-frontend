bc2898b0403294a4f641197cb3f08820de028564
Jniemel <juho_n@live.fi>
2024-05-06 15:37:44 +0300

    teams characters now appear on map

--- src/game/scenes/game.js
+++ src/game/scenes/game.js
@@ -13,12 +13,35 @@ export default class Game extends Phaser.Scene {
 
   create() {
     console.log(this.state);
+    // create map
     const map = this.make.tilemap({ key: 'arena-1' });
     const tileset = map.addTilesetImage('arena', 'tiles');
-
     map.createLayer('Ground', tileset);
     const wallsLayer = map.createLayer('Walls', tileset);
-
     wallsLayer.setCollisionByProperty({ collides: true });
+
+    // place teams
+    placeTeam(this.state.south, 'south', 576, this);
+    placeTeam(this.state.north, 'north', 32, this);
   }
+}
+
+function placeTeam(champs, team, yPos, self) {
+  let y = yPos;
+  let x = 192;
+  let count = 1;
+  champs.forEach((champ) => {
+    if (count === 4 && team === 'south') {
+      y -= 32;
+      x = 192;
+    } else if (count === 4 && team === 'north') {
+      y += 32;
+      x = 192;
+    }
+    const cName = champ.class.charAt(0).toLowerCase() + champ.class.slice(1);
+    self.add.sprite(x, y, cName).setOrigin(0, 0);
+    console.log(cName);
+    x += 32;
+    count += 1;
+  });
 }
--- src/game/scenes/preloader.js
+++ src/game/scenes/preloader.js
@@ -6,8 +6,13 @@ export default class Preloader extends Phaser.Scene {
   }
 
   preload() {
+    // load tiles and tilemaps
     this.load.image('tiles', './assets/game/tiles/tiles.png');
     this.load.tilemapTiledJSON('arena-1', './assets/game/maps/div4.json');
+    // load class images
+    this.load.image('swordsman', './assets/game/characters/swordsman.png');
+    this.load.image('rogue', './assets/game/characters/rogue.png');
+    this.load.image('bandit', './assets/game/characters/bandit.png');
   }
 
   create(data) {
